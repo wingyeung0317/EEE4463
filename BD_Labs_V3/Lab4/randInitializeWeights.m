37367fe8edb9a5e3056117afa86421362c4505cf
Wing <admin@killicit.com>
2024-07-04 15:15:31 +0800

    Lab4: v.Model_Answer

--- BD_Labs_V3/Lab4/randInitializeWeights.m
+++ BD_Labs_V3/Lab4/randInitializeWeights.m
@@ -1,35 +1,27 @@
-function W = randInitializeWeights(L_in, L_out, seed)
-%randInitializeWeights Randomly initialize the weights of a layer with L_in
+function W = randInitializeWeights(L_in, L_out)
+%RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
 %incoming connections and L_out outgoing connections
-%   W = randInitializeWeights(L_in, L_out, seed) randomly initializes the weights 
-%   of a layer with L_in incoming connections and L_out outgoing 
-%   connections. If seed is provided, rand's seed will be set to it.
+%   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights
+%   of a layer with L_in incoming connections and L_out outgoing
+%   connections.
 %
 %   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
-%   the first column of W handles the "bias" terms
+%   the first row of W handles the "bias" terms
 %
-if exist('seed', 'var')
-    fprintf('\nrand seed=%f', seed);
-    rand("seed", seed);
-end
-% You need to return the following variables correctly 
-W = zeros(L_out, 1 + L_in);
 
+% You need to return the following variables correctly
+W = zeros(L_out, 1 + L_in);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Initialize W randomly so that we break the symmetry while
 %               training the neural network.
 %
-% Note: The first column of W corresponds to the parameters for the bias unit
+% Note: The first row of W corresponds to the parameters for the bias units
 %
-% Check hints in worksheet
 
 
-
-
-
-
-
+epsilon_init = 0.12;
+W = rand(L_out, 1+L_in)*2*epsilon_init - epsilon_init;
 
 
 % =========================================================================
--- BD_Labs_V3/Lab4/sigmoidGradient.m
+++ BD_Labs_V3/Lab4/sigmoidGradient.m
@@ -1,7 +1,7 @@
 function g = sigmoidGradient(z)
-%sigmoidGradient returns the gradient of the sigmoid function
+%SIGMOIDGRADIENT returns the gradient of the sigmoid function
 %evaluated at z
-%   g = sigmoidGradient(z) computes the gradient of the sigmoid function
+%   g = SIGMOIDGRADIENT(z) computes the gradient of the sigmoid function
 %   evaluated at z. This should work regardless if z is a matrix or a
 %   vector. In particular, if z is a vector or matrix, you should return
 %   the gradient for each element.
@@ -11,15 +11,11 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
-%
-% Check hints in worksheet
 
-
-
+g  = sigmoid(z).*(1-sigmoid(z));
 
 
 % =============================================================
-
 
 
 
